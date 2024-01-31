c9d3aecd8333e4ebc8bd518c42510052911c8ef0
tete123456 <xotjd794613@naver.com>
2024-01-31 21:36:24 +0900

    first setting

---
+++ src/main/java/com/teamTS/funfun/FunfunApplication.java
@@ -0,0 +1,14 @@
+package com.teamTS.funfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunfunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunfunApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/teamTS/funfun/FunfunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamTS.funfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunfunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
